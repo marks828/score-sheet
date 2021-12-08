e7b75d0d94607fe340101866d4e80d1692fa8bb4
mark <marks828@gmail.com>
2021-12-08 14:48:12 -0500

    update

--- src/scoresheet/scoreHome.js
+++ src/scoresheet/scoreHome.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 export function ScoreHome(){
     
     const [numberOfPlayersSelected, setNumberOfPlayersSelected] = useState(0);
-    const [inputValue, setInputValue] = useState('');
+    const [numberOfScoreSheets, setNumberOfScoreSheets] = useState(numberOfPlayersSelected);
     
     function handleNumberOfPlayersSelectedSubmit(e){
         e.preventDefault();
@@ -15,13 +15,12 @@ export function ScoreHome(){
         <>
         <label htmlFor="numberOfPlayersSelected">Number of Players:
             <input
-                type="text" 
+                type="number" 
                 name="numberOfPlayersSelected"
                 value={numberOfPlayersSelected}
-                onChange={setNumberOfPlayersSelected}
+                onChange={handleNumberOfPlayersSelectedSubmit}
             />
         </label>
-        
         <h1>Scores:</h1>
         <p>{numberOfPlayersSelected}</p>
         
